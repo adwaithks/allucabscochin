5904857db2b380a1ceb9428bd76650c7eab9e31f
adwaithks <adwaith2507@gmail.com>
2022-03-10 00:09:08 +0530

    banner image

--- src/custom/components/Float/Float.jsx
+++ src/custom/components/Float/Float.jsx
@@ -6,7 +6,7 @@ function Float() {
   return (
     <div className='floater'>
         <BsWhatsapp onClick={() => {
-            window.open('https://web.whatsapp.com/send?phone=+918590868047&text=Enquiring%20for%20available%20services%20-%20Allu%20Cabs%20Cochin', "_blank")   
+            window.open('https://api.whatsapp.com/send?phone=+918590868047&text=Enquiring%20for%20available%20services%20-%20Allu%20Cabs%20Cochin', "_blank")   
         }} />
     </div>
   )
--- src/custom/components/Footer/Footer.jsx
+++ src/custom/components/Footer/Footer.jsx
@@ -18,7 +18,7 @@ function Footer() {
 
                     }} />
                     <BsWhatsapp className='social-link-whats'  onClick={() => {
-                        window.open('https://web.whatsapp.com/send?phone=+918590868047&text=Enquiring%20for%20available%20services%20-%20Allu%20Cabs%20Cochin', "_blank")
+                        window.open('https://api.whatsapp.com/send?phone=+918590868047&text=Enquiring%20for%20available%20services%20-%20Allu%20Cabs%20Cochin', "_blank")
                     }} />
                     <BsInstagram className='social-link-insta' onClick={() => {
                         
