25e7245e30c02db0dc0f241f185484d223fd417b
adwaithks <adwaith2507@gmail.com>
2022-03-10 22:22:13 +0530

    final

--- src/custom/components/PickTaxi/PickTaxi.jsx
+++ src/custom/components/PickTaxi/PickTaxi.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import './PickTaxi.scss';
 import { Carousel } from 'react-responsive-carousel';
 import {AiOutlineLine} from 'react-icons/ai';
+import {GoPrimitiveDot} from 'react-icons/go';
 
 function PickTaxi() {
 
@@ -119,35 +120,43 @@ function PickTaxi() {
                         swipeable={true}
                         showStatus={false}
                         showArrows={true}
+                        showIndicators={false}
                         autoFocus={true}
                     >   
                             <div className='picktaxi-img-wrap-mob'>
                                 <img className='picktaxi-img-mob' src={IMAGES[0].src} loading="lazy" alt="" />
-                                <h4 className='picktaxi-img-desc-mob'>Innova Crista <AiOutlineLine className='line-sep' /> First 100 KM Rs.2000 <AiOutlineLine className='line-sep' /> Extra KM Rs.20/KM</h4>
+                                <h4 className='picktaxi-img-desc-mob-top'>Innova Crista</h4>
+                                <h4 className='picktaxi-img-desc-mob-down'>First 100 KM Rs.2000 <GoPrimitiveDot /> Extra KM Rs.20/KM</h4>
                             </div>
                             <div className='picktaxi-img-wrap-mob'>
                                 <img className='picktaxi-img-mob' src={IMAGES[1].src} loading="lazy" alt="" />
-                                <h4 className='picktaxi-img-desc-mob'>Etios <AiOutlineLine className='line-sep' /> First 100 KM Rs.1300 <AiOutlineLine className='line-sep' /> Extra KM Rs.13/KM</h4>
+                                <h4 className='picktaxi-img-desc-mob-top'>Etios</h4>
+                                <h4 className='picktaxi-img-desc-mob-down'>First 100 KM Rs.1300 <GoPrimitiveDot /> Extra KM Rs.13/KM</h4>
                             </div>
                             <div className='picktaxi-img-wrap-mob'>
                                 <img className='picktaxi-img-mob' src={IMAGES[2].src} loading="lazy" alt="" />
-                                <h4 className='picktaxi-img-desc-mob'>Swift Dzire <AiOutlineLine className='line-sep' /> First 100 KM Rs.1300 <AiOutlineLine className='line-sep' /> Extra KM Rs.13/KM</h4>
+                                <h4 className='picktaxi-img-desc-mob-top'>Swift Dzire</h4>
+                                <h4 className='picktaxi-img-desc-mob-down'>First 100 KM Rs.1300 <GoPrimitiveDot /> Extra KM Rs.13/KM</h4>
                             </div>
                             <div className='picktaxi-img-wrap-mob'>
                                 <img className='picktaxi-img-mob' src={IMAGES[3].src} loading="lazy" alt="" />
-                                <h4 className='picktaxi-img-desc-mob'>Force Traveller <AiOutlineLine className='line-sep' /> First 100 KM Rs.2700 <AiOutlineLine className='line-sep' /> Extra KM Rs.18/KM</h4>
+                                <h4 className='picktaxi-img-desc-mob-top'>Force Traveller</h4>
+                                <h4 className='picktaxi-img-desc-mob-down'>First 100 KM Rs.2700 <GoPrimitiveDot /> Extra KM Rs.18/KM</h4>
                             </div>
                             <div className='picktaxi-img-wrap-mob'>
                                 <img className='picktaxi-img-mob' src={IMAGES[4].src} loading="lazy" alt="" />
-                                <h4 className='picktaxi-img-desc-mob'>Ertiga <AiOutlineLine className='line-sep' /> First 100 KM Rs.1500 <AiOutlineLine className='line-sep' /> Extra KM Rs.15/KM</h4>
+                                <h4 className='picktaxi-img-desc-mob-top'>Ertiga</h4>
+                                <h4 className='picktaxi-img-desc-mob-down'>First 100 KM Rs.1500 <GoPrimitiveDot /> Extra KM Rs.15/KM</h4>
                             </div>  
                             <div className='picktaxi-img-wrap-mob'>
                                 <img className='picktaxi-img-mob' src={IMAGES[5].src} loading="lazy" alt="" />
-                                <h4 className='picktaxi-img-desc-mob'>Innova <AiOutlineLine className='line-sep' /> First 100 KM Rs.1700 <AiOutlineLine className='line-sep' /> Extra KM Rs.17/KM</h4>
+                                <h4 className='picktaxi-img-desc-mob-top'>Innova</h4>
+                                <h4 className='picktaxi-img-desc-mob-down'>First 100 KM Rs.1700 <GoPrimitiveDot /> Extra KM Rs.17/KM</h4>
                             </div>
                             <div className='picktaxi-img-wrap-mob'>
                                 <img className='picktaxi-img-mob' src={IMAGES[6].src} loading="lazy" alt="" />
-                                <h4 className='picktaxi-img-desc-mob'>Ford Aspire <AiOutlineLine className='line-sep' /> First 100 KM Rs.1300 <AiOutlineLine className='line-sep' /> Extra KM Rs.15/KM</h4>
+                                <h4 className='picktaxi-img-desc-mob-top'>Ford Aspire</h4>
+                                <h4 className='picktaxi-img-desc-mob-down'>First 100 KM Rs.1300 <GoPrimitiveDot /> Extra KM Rs.15/KM</h4>
                             </div>
                         </Carousel> 
                     </div>
